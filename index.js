53d0182e22ef6d102ca2f12787151995fc42ee33
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-03-28 14:01:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist){
+  let results;
+  if(dist <= 400){
+    results = 'This one is on me!';
+    return results;
+  }else if((dist > 2000) && (dist < 2500)) {
+    results = 'I will gladly take your thirty bucks.';
+    return results;
+  }else {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let results = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return results;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+
+  }
+
 }
+
